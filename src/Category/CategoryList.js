0be89d238d1d427653ac8fad7c30038562badb58
djole994 <djordjeradovic94@gmail.com>
2025-03-19 09:20:10 +0100

    api/category

---
+++ src/Category/CategoryList.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import api from "../Api";
+import "./CategoryList.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faLaptop,
+    faMobileAlt,
+    faMicrochip,
+    faGamepad,
+    faHome,
+    faTv,
+    faHeadphonesAlt,
+    faNetworkWired,
+    faClock, //  za faWatch
+    faPlug
+  } from "@fortawesome/free-solid-svg-icons";
+  
+
+// Mapiranje kategorija na ikonice
+const iconMapping = {
+  "Laptops & Notebooks": faLaptop,
+  "Smartphones & Tablets": faMobileAlt,
+  "Computer Components": faMicrochip,
+  "Gaming Consoles": faGamepad,
+  "Smart Home Devices": faHome,
+  "TV & Home Theater": faTv,
+  "Audio & Headphones": faHeadphonesAlt,
+  "Networking Equipment": faNetworkWired,
+  "Wearable Tech (Smartwatches, etc.)": faClock,
+  "Accessories (Cables, Chargers, Cases)": faPlug
+};
+
+const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
+  // Dohvaćanje kategorija s API-a
+  const fetchCategories = async () => {
+    try {
+      const response = await api.get("/Category");
+      setCategories(response.data);
+    } catch (error) {
+      console.error("Greška pri dohvatanju kategorija:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return (
+    <div className="category-container">
+      <div className="category-grid">
+        {categories.map((cat) => (
+          <div key={cat.id} className="category-card">
+            <div className="icon-wrapper">
+              {iconMapping[cat.name] ? (
+                <FontAwesomeIcon icon={iconMapping[cat.name]} size="2x" />
+              ) : (
+                <img
+                  alt={cat.name}
+                  src={`http://localhost:5199${cat.imageUrl}`}
+                  className="category-image"
+                />
+              )}
+            </div>
+            <h3>{cat.name}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CategoryList from '../src/Category/CategoryList';
+
+const Home = () => {
+  return (
+    <div>
+      <CategoryList />
+    </div>
+  );
+};
+
+export default Home;
